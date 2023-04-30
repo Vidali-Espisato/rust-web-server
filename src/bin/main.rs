d82479d1b316e5a007957470f8aaafc978bbbf7e
Manish Roy <manish.11612939@lpu.in>
2023-04-30 20:01:30 +0530

    moved main crate inside bin and updated for multi-threading

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,14 +1,21 @@
-use std::fs;
+use std::time::Duration;
+use std::{fs, thread};
+use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use basic_rust_server::ThreadPool;
 
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_stream(stream);
+        pool.execute(|| {
+            handle_stream(stream)
+        });
     }
 }
 
@@ -18,9 +25,13 @@ fn handle_stream(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let ( status_line, filename ) =
         if buffer.starts_with(get) {
+            ("200 OK", "static/index.html")
+        } else if buffer.starts_with(sleep) {
+            thread::sleep(Duration::from_secs(15));
             ("200 OK", "static/index.html")
         } else {
             ("404 NOT FOUND", "static/error.html")
